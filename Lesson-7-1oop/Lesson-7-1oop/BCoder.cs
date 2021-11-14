9bc962a828dbcb32015423ccde73686271936d82
Сергей <gan7er@yandex.ru>
2021-11-14 11:51:41 +0300

    add interface and class

---
+++ Lesson-7-1oop/Lesson-7-1oop/BCoder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_7_1oop
+{
+    public class BCoder : ICoder
+    {
+        public string Decode(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Encode(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Lesson-7-1oop/Lesson-7-1oop/ICoder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_7_1oop
+{
+   public interface ICoder
+    {
+        string Encode(string input);
+        string Decode(string input);
+    }
+}
